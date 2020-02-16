300967a6946c427a33cd1c5351bd5b5130146ad3
Goryudyuma <umaretekyoumade@gmail.com>
2020-02-16 20:43:28 +0900

    calcChange

--- hello.hs
+++ hello.hs
@@ -1,2 +1,13 @@
 main = do
   print "Hello World!"
+
+simple x = x
+
+calcChange owed given =
+  if change > 0
+    then change
+    else 0
+  where
+    change = given - owed
+
+doubleDouble t = (\x -> x * 2) ( (\x -> x * 2) t)
