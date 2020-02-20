321922f83aed12059fd106b02f987901c2433d5e
Goryudyuma <umaretekyoumade@gmail.com>
2020-02-20 23:26:17 +0900

    Lesson 7

--- hello.hs
+++ hello.hs
@@ -15,3 +15,13 @@ doubleDouble t = (\x -> x * 2) ((\x -> x * 2) t)
 teams = ["red", "yellow", "orange", "blue", "purple"]
 
 isPalindrome word = word == reverse word
+
+myTail aList =
+  case aList of
+    (x:xs) -> xs
+    _ -> []
+
+myGCD a b =
+  case (a, b) of
+    (a, 0) -> a
+    (a, b) -> myGCD b $ a `mod` b
