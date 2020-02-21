b8d6efa5cf339718b92a4f5288aceb6408a0b856
Goryudyuma <umaretekyoumade@gmail.com>
2020-02-21 23:10:31 +0900

    マイナスになることはない

--- cup.hs
+++ cup.hs
@@ -1,3 +1,11 @@
 cup flOz = \message -> message flOz
 
 getOz aCup = aCup (\flOz -> flOz)
+
+drinc aCup ozDrank =
+  if ozDiff >= 0
+    then cup (ozDiff)
+    else cup 0
+  where
+    flOz = getOz aCup
+    ozDiff = flOz - ozDrank
