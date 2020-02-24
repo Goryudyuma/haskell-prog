4583793c8c442d3d5e4062acb49eebe5c8555f6b
Goryudyuma <umaretekyoumade@gmail.com>
2020-02-24 23:32:36 +0900

    BloodType

--- type.hs
+++ type.hs
@@ -22,6 +22,32 @@ sexInitial :: Sex -> Char
 sexInitial Male = 'M'
 sexInitial Female = 'F'
 
+data RhType
+  = Pos
+  | Neg
+
+data ABOType
+  = A
+  | B
+  | AB
+  | O
+
+data BloodType =
+  BloodType ABOType RhType
+
+showRh :: RhType -> String
+showRh Pos = "+"
+showRh Neg = "-"
+
+showABO :: ABOType -> String
+showABO A = "A"
+showABO B = "B"
+showABO AB = "AB"
+showABO O = "O"
+
+showBloodType :: BloodType -> String
+showBloodType (BloodType abo rh) = showABO abo ++ showRh rh
+
 patientInfo :: PatientName -> Age -> Height -> String
 patientInfo patient age height = name ++ " " ++ ageHeight
   where
