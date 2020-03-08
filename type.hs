5d3a567e1ae485d4eea8873fa11a86b731c80b16
Goryudyuma <umaretekyoumade@gmail.com>
2020-03-08 15:08:42 +0900

    cycleSucc

--- type.hs
+++ type.hs
@@ -77,3 +77,9 @@ data Icecream
   = Chocolate
   | Vanilla
   deriving (Show, Eq, Ord)
+
+cycleSucc :: (Bounded a, Enum a, Eq a) => a -> a
+cycleSucc n =
+  if n == maxBound
+    then minBound
+    else succ n
